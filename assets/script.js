b5ceb86964ed3b1ec77bed0f0213e08b64a24265
James A Cone <jacone626@gmail.com>
2023-03-02 14:31:54 -0500

    continuing to work on the API key. It loads but is messing up when I include a variable within it

--- assets/script.js
+++ assets/script.js
@@ -1,27 +1,24 @@
-var apiKey = "c97f1cd354b9dea64133acfd0f866f22";
-var city = $("#city-input").val();
-// var requestUrl = 'api.openweathermap.org/data/2.5/weather?q=' + city + '&appid=' + apiKey;
 
-var requestUrl = 'http://api.openweathermap.org/data/2.5/weather?q=London&APPID=c97f1cd354b9dea64133acfd0f866f22';
-
-fetch(requestUrl)
-  .then(function (response) {
-    return response.json();
-  })
-  .then(function (data) {
-    console.log(data);
-    }
-  );
-
-  
-  $("#search-button").click(function(event){
+var searchCity = function() {
+    $("#search-button").click(function(event){
     event.preventDefault();
 
     var city = $("#city-input").val();
 
     console.log("city name:", city);
-  })
 
+    var requestUrl = 'api.openweathermap.org/data/2.5/weather?q=' + city + '&appid=c97f1cd354b9dea64133acfd0f866f22';
+    fetch(requestUrl)
+        .then(function (response) {
+        return response.json();
+    })
+        .then(function (data) {
+        console.log(data);
+    });
+  })
+}
+
+searchCity();
 
 
 $(function(){
