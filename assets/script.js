a4257e52db59b4a3c79518a754d071bca0000700
James A Cone <jacone626@gmail.com>
2023-03-02 16:14:18 -0500

    added current date to js

--- assets/script.js
+++ assets/script.js
@@ -1,27 +1,36 @@
 
+
 var searchCity = function() {
     $("#search-button").click(function(event){
     event.preventDefault();
 
-    var city = $("#city-input").val();
+    var city = $("#city-input").val().trim();
 
     console.log("city name:", city);
 
-    var requestUrl = 'api.openweathermap.org/data/2.5/weather?q=' + city + '&appid=c97f1cd354b9dea64133acfd0f866f22';
+    var requestUrl = 'http://api.openweathermap.org/data/2.5/weather?q=' + city + '&appid=c97f1cd354b9dea64133acfd0f866f22';
     fetch(requestUrl)
         .then(function (response) {
-        return response.json();
-    })
-        .then(function (data) {
-        console.log(data);
-    });
+        if (response.ok) {
+            response.json().then(function (data) {
+            console.log(data);
+            document.getElementById("current-city").textContent = city + " (" + dayjs().format('MM/D/YYYY') + ")"
+            
+            });
+        } else {
+            alert("City not found")
+        }
   })
+})
 }
 
+
 searchCity();
+
 
 
 $(function(){
 
     
 });
+
