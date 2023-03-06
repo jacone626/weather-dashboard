f7ec344e0421957b663ee6dbfb63e5509211ed08
James A Cone <jacone626@gmail.com>
2023-03-06 10:27:08 -0500

    working on API key for daily weather forecast

--- assets/script.js
+++ assets/script.js
@@ -21,12 +21,30 @@ var searchCity = function() {
             document.getElementById("current-wind").textContent = "Wind: " + data.wind.speed + " mph"
             document.getElementById("current-humidity").textContent = "Humidity: " + data.main.humidity + " %"
             //Show future weather
-
+            var futureAPI = "https://api.openweathermap.org/data/2.5/forecast/daily/?q=" + city + "&cnt=5&appid=c97f1cd354b9dea64133acfd0f866f22&units=imperial";
+            fetch(futureAPI)
+                .then(function (response) {
+                response.json().then(function (data) {
+                console.log(data);
+                })
+            })
             });
         } else {
             alert("City not found")
         }
   })
+}
+
+function showFiveDay (data) {
+    for (var i=0; i<5; i++) {
+        var weatherData = {
+            date: "not sure yet",
+            icon: "http://openweathermap.org/img/w/" + data.list[i++].weather[0].icon + ".png",
+            temp: data.list[i++].main.temp,
+            wind: data.list[i++].wind.speed,
+            humidity: data.list[i++].humidity
+        }
+    }
 }
 
 var searchedCityButton = function() {
