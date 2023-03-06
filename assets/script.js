b8506328e0dae27ef1b462be69658776e2c1d82e
James A Cone <jacone626@gmail.com>
2023-03-06 15:52:15 -0500

    got current weather to appear again after moving it to a new function. Working on getting future weather to show up

--- assets/script.js
+++ assets/script.js
@@ -1,5 +1,5 @@
 
-var searchCity = function() {
+function searchCity () {
 
     var city = $("#city-input")[0].value.trim();
 
@@ -10,25 +10,26 @@ var searchCity = function() {
         .then(function (response) {
         if (response.ok) {
             response.json().then(function (data) {
-            console.log(data);
+                console.log(data);
             //Add city to list
-            $("#searched-cities").append('<li type="button" class="btn bg-secondary mt-2 form-control">' + city);
-            //Set local storage with the latitude and longitude
-            var lat = data.coord.lat;
-            var lon = data.coord.lon;
-
-            var latAndLon = lat.toString() + " , " + lon.toString();
-
-            localStorage.setItem(city, latAndLon)
+                $("#searched-cities").append('<li type="button" class="btn bg-secondary mt-2 form-control">' + city);
             //Show current city
-            $("#current-city")[0].textContent = city + " (" + dayjs().format('MM/D/YYYY') + ")"
+                $("#current-city")[0].textContent = city + " (" + dayjs().format('MM/D/YYYY') + ")"
+            //Set local storage with the latitude and longitude
+                var lat = data.coord.lat;
+                var lon = data.coord.lon;
+
+                var latAndLon = lat.toString() + " , " + lon.toString();
+
+                localStorage.setItem(city, latAndLon)
+
             //Show future weather
-            var futureAPI = "http://api.openweathermap.org/data/2.5/onecall?lat=" + lat + "&lon=" + lon + "&exclude=minutely,hourly&units=imperial&appid=71311474f5b26fb7bbfa0bc1985b90cd";
-            fetch(futureAPI)
-                .then(function (futureResponse) {
-                futureResponse.json().then(function (futureData) {
-                console.log(futureData);
-                currentWeather(futureData);
+                var futureAPI = "http://api.openweathermap.org/data/2.5/onecall?lat=" + lat + "&lon=" + lon + "&exclude=minutely,hourly&units=imperial&appid=71311474f5b26fb7bbfa0bc1985b90cd";
+                fetch(futureAPI)
+                    .then(function (futureResponse) {
+                    futureResponse.json().then(function (futureData) {
+                        console.log(futureData);
+                        currentWeather(futureData);
                 })
             })
             });
@@ -40,41 +41,37 @@ var searchCity = function() {
 
 function currentWeather (data) {
 
-   $("#current-weather-icon")[0].src = "http://openweathermap.org/img/w/" + data.weather[0].icon + ".png"
-   $("#current-temp")[0].textContent = "Temperature: " + data.main.temp + " °F"
-   $("#current-wind")[0].textContent = "Wind: " + data.wind.speed + " mph"
-   $("#current-humidity")[0].textContent = "Humidity: " + data.main.humidity + " %"
+   $("#current-weather-icon")[0].src = "http://openweathermap.org/img/w/" + data.current.weather[0].icon + ".png";
+   $("#current-temp")[0].textContent = "Temperature: " + data.current.temp + " °F";
+   $("#current-wind")[0].textContent = "Wind: " + data.current.wind_speed + " mph";
+   $("#current-humidity")[0].textContent = "Humidity: " + data.current.humidity + " %";
+
+   showFiveDay(data);
 }
 
 function showFiveDay (data) {
-    document.getElementById("temp-1").textContent = "Temperature: " + data.daily[1].temp.day + " °F"
+   
     for (var i=1; i<6; i++) {
-        var weatherData = {
-            date: "not sure yet",
-            icon: "http://openweathermap.org/img/w/" + data.daily[i++].weather[0].icon + ".png",
-            temp: data.daily[i++].temp.day,
-            wind: data.daily[i++].wind_speed,
-            humidity: data.daily[i++].humidity
-        }
+        
         var currentDate = "#date-" + i;
-        $(currentDate)[1].textContent = weatherData.date;
-        currentImg= "#img-" + i;
-        $(currentImg)[1].src = weatherData.icon;
-        currentTemp= "#temp-" + i;
-        $(currentTemp)[1].textContent = "Wind: " + weatherData.temp + " mph"
-        currentWind= "#wind-" + i;
-        $(currentWind)[1].textContent = "Temp: " + weatherData.temp + " °F"
-        currentHumidity = "#humidity-" + i;
-        $(currentHumidity)[1].textContent = "Humidity: " + weatherData.humidity + "%";
+        $(currentDate)[0].textContent = "pending";
+        var currentImg= "#img-" + i;
+        $(currentImg)[0].src = "http://openweathermap.org/img/w/" + data.daily[i+1].weather[0].icon + ".png";
+        var currentTemp= "#temp-" + i;
+        $(currentTemp)[0].textContent = "Temp " + data.daily[i+1].temp.day + " °F";
+        var currentWind= "#wind-" + i;
+        $(currentWind)[0].textContent = "Wind: " + data.daily[i+1].wind_speed + " mph";
+        var currentHumidity = "#humidity-" + i;
+        $(currentHumidity)[0].textContent = "Humidity: " + data.daily[i+1].humidity+ "%";
     }
 
 
 }
 
-var searchedCityButton = function() {
+// var searchedCityButton = function() {
    
 
-}
+// }
 
 
 $("#search-button").click(function(event){
@@ -82,10 +79,10 @@ $("#search-button").click(function(event){
     searchCity();
 })
 
-$("#searched-cities").click(function() {
+// $("#searched-cities").click(function() {
 
 
-})
+// })
 
 
 
